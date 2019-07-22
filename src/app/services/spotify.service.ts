df034ce736e07cf188e10c85c5f2a9a922bba35b
GerCoss <gerardocoss92@gmail.com>
2019-07-22 08:34:57 -0500

    siguiendo el curso de angular

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +12,36 @@ export class SpotifyService {
     console.log('spotiy listo');
   }
 
-  getNewReleases() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCceyP23Zjt_cmC9EnvskwI6--pzBUTCHsZ4X2_Y5ZpQs7-C1R_2KtyIhVOAnc3k9YVpHxyZFXZXcse6EY'
+      'Authorization': 'Bearer BQCJzhxYSCoDsQbac3SfsBsxJDWW5nqG-x8f1PNoUqrQLUP1HBXLQCEnWHk_LhMqoA_6n8FpXHADH1ubU6Y'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+
+    // const headers = new HttpHeaders({
+    //   'Authorization': 'Bearer BQCJzhxYSCoDsQbac3SfsBsxJDWW5nqG-x8f1PNoUqrQLUP1HBXLQCEnWHk_LhMqoA_6n8FpXHADH1ubU6Y'
+    // });
+
+    return this.getQuery('browse/new-releases?limit=20')
+    // this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+              .pipe( map( data => data['albums'].items));
+  }
+
+  getArtista(termino: string){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQCJzhxYSCoDsQbac3SfsBsxJDWW5nqG-x8f1PNoUqrQLUP1HBXLQCEnWHk_LhMqoA_6n8FpXHADH1ubU6Y'
+    });
+
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+    // this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, { headers })
+              .pipe( map( data => data['artists'].items));
+
   }
 }
